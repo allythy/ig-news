ae38320b168749b280bae7141533d9f3e10d280f
Allythy <allythy@protonmail.com>
2021-09-25 17:20:59 -0300

    Dynamic title from page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
 import styles from '../styles/home.module.scss'
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <h2 className={styles.title}>
-      Hello<span>word</span>
-    </h2>   
+    <>
+      <Head>
+        <title>Inicio | Ignws</title>
+      </Head>
+      <h2 className={styles.title}>
+        Hello<span>word</span>
+      </h2>
+    </>
   )
 }
  
