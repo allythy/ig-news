c9f8600263f85eef2885c216885e67f9c139162a
Allythy <allythy@protonmail.com>
2021-10-02 15:04:16 -0300

    Create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,24 @@
 import Head from 'next/head';
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | Ignws</title>
+        <title>Home | Ignws</title>
       </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>Hey, Welcome</span>
+          <h1>News about the <span>React</span> word</h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 moth</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="" />
+      </main>
     </>
   )
 }
