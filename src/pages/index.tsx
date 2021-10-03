f72b05fed6a74fd790f0ea2f5926570877af3e9f
Allythy <allythy@protonmail.com>
2021-10-02 21:53:40 -0300

    Consume stripe API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss'
+import { stripe } from '../services/stripe'
 
-export default function Home() {
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amout: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -15,13 +25,29 @@ export default function Home() {
           <h1>News about the <span>React</span> word</h1>
           <p>
             Get acess to all the publications <br />
-            <span>for $9.90 moth</span>
+            <span>for {product.amout} moth</span>
           </p>
-          <SubscribeButton/>
+          <SubscribeButton priceId={product.priceId}/>
         </section>
         <img src="/images/avatar.svg" alt="" />
       </main>
     </>
   )
 }
- 
+
+export const getServerSideProps: GetServerSideProps = async () =>{
+  const price  = await stripe.prices.retrieve('price_1JgGY1IIgrZnJly2nt24T8KD')
+
+  const product  = {
+    priceId: price.id,
+    amout: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  }
+  return{
+    props: {
+      product,
+    }
+  }
+}
